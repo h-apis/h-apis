802f83acedd5affc8f34246b15a1949694b88d78
hassan <fakku-you@hitomi.la>
2020-10-25 17:11:08 +0900

    [chore] 테스트 전장정리 로직 추가

--- src/core/hitomi/service.spec.ts
+++ src/core/hitomi/service.spec.ts
@@ -1,4 +1,5 @@
 import * as hitomi from './service';
+import fse from 'fs-extra';
 import {assert} from 'chai';
 
 describe('hitomi', () => {
@@ -9,24 +10,27 @@ describe('hitomi', () => {
 
     it('getGalleryMetadata', async () => {
         const result = await hitomi.getGalleryMetaData(1761941);
-        console.log(result);
+        // console.log(result);
         assert.isObject(result);
     });
 
     it('getGalleryMetadataFromHitomi', async () => {
         const result = await hitomi.getGalleryMetaDataFromHitomi(1761941);
-        console.log(result);
+        // console.log(result);
         assert.isObject(result);
     });
 
     it('downloadImage', async function () {
         this.timeout(50000);
         const dummyUrl = 'https://ab.hitomi.la/images/0/72/1923b3e2d6e5b904617ad54a9140c5017d1014910967b62d99bc98d0488eb720.jpg';
+        const targetImageFileUrl = './image.jpg';
         try {
-            await hitomi.downloadImage(dummyUrl, './image.jpg');
+            await hitomi.downloadImage(dummyUrl, targetImageFileUrl);
             assert.ok('downloaded')
         } catch (e) {
             assert.fail(e)
+        } finally {
+            fse.removeSync(targetImageFileUrl);
         }
     })
 })
