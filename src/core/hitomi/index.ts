5bdd9c0e5aceb83c931d4b5fb55137607c7e5ca1
hassan <fakku-you@hitomi.la>
2020-10-25 17:03:26 +0900

    [feature] hitomi 다운시 특수문자에 막혀서 에러발생하는 문제 수정

--- src/core/hitomi/index.ts
+++ src/core/hitomi/index.ts
@@ -15,12 +15,23 @@ class HitomiCore {
     async download(galleryNumber: number) {
         const {title, files} = await hitomiService.getGalleryMetaDataFromHitomi(galleryNumber);
         const imageUrlList = files.map((file) => hitomiService.getImageUrlFrom(galleryNumber, file));
-        const targetDirectory = path.join(this.directory, title);
+        const targetDirectory = path.join(this.directory, this.escapeForDirectory(title));
 
         await fse.ensureDir(targetDirectory);
-        await Promise.all(imageUrlList.map((url, index) => {
-            hitomiService.downloadImage(url, path.join(targetDirectory, files[index].name));
-        }));
+        await Promise.all(imageUrlList.map((url, index) =>
+            hitomiService.downloadImage(url, path.join(targetDirectory, files[index].name))
+        ));
+    }
+
+    /**
+     * 디렉토리명에 특수문자가 들어갈 수 없으므로, 전부 + 처리한다.
+     * 추가로 공백은 _ 처리한다.
+     * @private
+     */
+    private escapeForDirectory(target: string) {
+        return target
+            .replace(/[ ]/g, '_')
+            .replace(/[&\/\\#,+()$~%.'":*?<>{}|]/g,'+')
     }
 }
 
